b241b37674642c15b1bd1391b35cc5e0240542a7
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2026-08-31 13:50:06 +0200

    Update camera.py
    fixing rotate

--- camera.py
+++ camera.py
@@ -13,17 +13,17 @@ class Camera4D:
 
     def rotate(self, plane: string, angle):
         if plane == "XY":
-            rot_result = transform.XY(normal_vector, angle)
+            rot_result = transform.XY(self.normal_vector, angle)
         elif plane == "XZ":
-            rot_result = transform.XZ(normal_vector, angle)
+            rot_result = transform.XZ(self.normal_vector, angle)
         elif plane == "XW":
-            rot_result = transform.XW(normal_vector, angle)
+            rot_result = transform.XW(self.normal_vector, angle)
         elif plane == "WZ":
-            rot_result = transform.WZ(normal_vector, angle)
+            rot_result = transform.WZ(self.normal_vector, angle)
         elif plane == "WY":
-            rot_result = transform.WY(normal_vector, angle)            
+            rot_result = transform.WY(self.normal_vector, angle)            
         elif plane == "ZY":
-            rot_result = transform.ZY(normal_vector, angle)
+            rot_result = transform.ZY(self.normal_vector, angle)
         else:
             print("Error: no such rotational plate, valid planes: XY, XZ, XW, WZ, WY, ZY")
 
@@ -31,5 +31,5 @@ class Camera4D:
             print("Error: unacceptable angle")
             return None
         else:
-            return Vector4(self.position , rot_result, self.distance)
+            return Camera4D(self.position , rot_result, self.distance)
             
